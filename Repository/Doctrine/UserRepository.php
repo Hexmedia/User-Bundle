9b0cf32353b91380bd58668d031d9677470f6a25
Krystian Kuczek <krystian@kuczek.eu>
2013-11-20 02:48:08 +0100

    Admin panel list's rewritten.

--- Repository/Doctrine/UserRepository.php
+++ Repository/Doctrine/UserRepository.php
@@ -4,25 +4,11 @@ namespace Hexmedia\UserBundle\Repository\Doctrine;
 
 use Doctrine\ORM\EntityRepository;
 use Hexmedia\UserBundle\Repository\UserRepositoryInterface;
+use Hexmedia\AdministratorBundle\Repository\Doctrine\ListTrait;
 
 class UserRepository extends EntityRepository implements UserRepositoryInterface {
 
-	public function getPage($page = 1, $sort = 'id', $pageSize = 10, $sortDirection = 'ASC', $fields = array()) {
-		$queryBuilder = $this->createQueryBuilder('u')
-			->setMaxResults($pageSize)
-			->setFirstResult(max(0, $page - 1) * $pageSize)
-			->orderBy('u.' . $sort, $sortDirection == 'ASC' ? 'ASC' : 'DESC')
-		;
-
-		return $queryBuilder->getQuery()->getResult();
-	}
-
-	public function getCount() {
-		$queryBuilder = $this->createQueryBuilder("u")
-			->select("count(u.id)");
-
-		return $queryBuilder->getQuery()->getSingleScalarResult();
-	}
+	use ListTrait;
 
 }
 
